3d71be6d067349ba72ebf410abde36dea5b35b14
Scott Schulz <swschulz@astrum.com>
2018-06-07 12:21:30 -0400

    Couple basic filters

--- home/.oh-my-custom/aliases.zsh
+++ home/.oh-my-custom/aliases.zsh
@@ -4,6 +4,8 @@ alias o='/usr/local/bin/officers'
 
 alias tl='todoist --project-namespace --namespace --color list'
 alias tcl='todoist --project-namespace --namespace --color completed-list'
+alias tlto="tl --filter \"(today | overdue)\""
+alias tltto="tl --filter \"(today | tomorrow | overdue)\""
 alias ta='todoist add'
 alias tm='todoist modify'
 alias ts='todoist sync'
